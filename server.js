5fdfcb7f5a01123caaafcbf6a269087de43f4df1
Ido Abramsohn <ido.abramsohn@gmail.com>
2021-02-25 14:26:47 -0500

    add skeletal routes for artwork model

--- server.js
+++ server.js
@@ -1,6 +1,60 @@
+// DEPENDENCIES //
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=> {
+// CONFIGURATION //
+
+// MIDDLEWARE
+
+// DATABASE SETTINGS //
+
+// MODELS //
+
+// CONTROLLERS //
+
+// ROUTES //
+// root
+app.get('/', (req, res) => {
+    res.send('root#get');
+});
+
+// index
+app.get('/artworks', (req, res) => {
+    res.send('index#get');
+});
+
+// new
+app.get('/artworks/new', (req, res) => {
+    res.send('new#get');
+});
+
+// create
+app.post('/artworks', (req, res) => {
+    res.send('create#post');
+});
+
+// show
+app.get('/artworks/:id', (req, res) => {
+    res.send('root#get');
+});
+
+// edit
+app.get('/artworks/:id/edit', (req, res) => {
+    res.send('root#get');
+});
+
+// update
+app.put('/artworks/:id', (req, res) => {
+    res.send('update#get');
+});
+
+// destroy
+app.delete('/artworks/:id', (req, res) => {
+    res.send('root#get');
+});
+
+
+app.listen(3000, () => {
     console.log('Express listening');
-})
+});
+
